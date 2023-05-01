363319ae6329809bdd4e923b83838c06997e89b7
gaanno <gabriel.norambuenam@gmail.com>
2023-05-01 02:29:14 -0400

    cambios imprimir tablero

--- Tablero.h
+++ Tablero.h
@@ -2,7 +2,7 @@
 #include <iostream>
 
 #define LARGO_TABLERO 15
-#define RANURA_VACIA '-'
+#define RANURA_VACIA '\0'
 
 using namespace std;
 
@@ -13,7 +13,7 @@ public:
     void imprimirTablero();
     void iniciarTableroManual();
     void iniciarTableroAutomatico();
-    void disparar();
+    void disparar(int fila, int columna);
 
 private:
     char tablero[LARGO_TABLERO][LARGO_TABLERO];
@@ -36,20 +36,42 @@ Tablero::Tablero()
 
 void Tablero::imprimirTablero()
 {
+    char letraFila = 'A';
+
     for (int fila = 0; fila < LARGO_TABLERO; fila++)
     {
+        cout << letraFila++ << "\t|\t";
         for (int columna = 0; columna < LARGO_TABLERO; columna++)
         {
             cout << this->tablero[fila][columna] << "\t";
         }
         cout << endl;
     }
+    for (int i = 0; i < LARGO_TABLERO + 2; i++)
+    {
+        cout << "-\t";
+    }
+    cout << endl;
+    for (int i = 0; i < LARGO_TABLERO + 2; i++)
+    {
+        if (i == 0 || i == 1)
+        {
+            cout << "\t";
+        }else{
+        cout << i - 2 << "\t";
+        }
+    }
+    cout << endl;
 }
 
-void Tablero::iniciarTableroManual(){
-
+void Tablero::iniciarTableroManual()
+{
 }
 
-void Tablero::iniciarTableroAutomatico(){
+void Tablero::iniciarTableroAutomatico()
+{
+}
 
+void Tablero::disparar(int fila, int columna)
+{
 }
--- servidor.cpp
+++ servidor.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cctype>
 
 #include "Tablero.h"
 #include "Barco.h"
