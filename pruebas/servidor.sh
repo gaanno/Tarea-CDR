6bed985ea9486871c3e3754cb103aac4cd49fbee
gaanno <gabriel.norambuenam@gmail.com>
2023-05-06 21:15:46 -0400

    modificaciones archivos de pruebas

--- pruebas/servidor.sh
+++ pruebas/servidor.sh
@@ -1,6 +1,10 @@
-clear
-cd ..
-rm -f servidor
-make servidor
+#!/bin/bash
+
+# cambia de ubicacion al directorio donde se encuentra el script
+clear && 
+cd $(dirname $0) && 
+cd .. &&
+rm servidor;
+make servidor &&
 ./servidor 50000
 
